d283f02885177b5d8d7b2bd83c86a1139ed1ebde
Minute PHP <dev@minutephp.com>
2015-11-22 00:48:55 +0530

    minor fixes

--- src/js/angular-markdown.js
+++ src/js/angular-markdown.js
@@ -1,18 +1,19 @@
 (function () {
     angular.module('angularMarkdown', ['ngSanitize'])
-/*
-        .config(function ($compileProvider) {
-            $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|ftp|mailto|file|javascript):/);
-        })
-*/
+        /*
+         .config(function ($compileProvider) {
+         $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|ftp|mailto|file|javascript):/);
+         })
+         */
         .service('$markdown', ['$sce', function ($sce) {
             var serviceInstance = {};
             var converter = new Showdown.converter();
 
             serviceInstance.markdownToHTML = function (text) {
                 var txt = (text || '').replace(/\r?\n/g, '<br/>');
-                var $text = $(converter.makeHtml(txt));
-                var html = $text.html() || '';
+                var convert = converter.makeHtml(txt);
+                var $text = $(convert);
+                var html = $text.html() || convert || '';
 
                 html = html.replace(/<soft-br\s*\/?>/g, ' <br class="hidden-xs hidden-sm" />');
                 html = html.replace(/<youtube>(?:.*?v=)?(.*?)<\/youtube>/g, '<div class="thumbnail"><div class="embed-responsive embed-responsive-4by3"><iframe width="560" height="315" src="https://www.youtube.com/embed/$1?rel=0&amp;controls=0&amp;hd=1&amp;showinfo=0" frameborder="0" allowfullscreen></iframe></div></div>');
